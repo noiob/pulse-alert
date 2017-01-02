7b6c17a4f521266917b626956d70825c05534c7e
Noiob <noiob@trillian.local>
2017-01-02 21:21:58 +0100

    Add a PayPal button to the settings page
    (someone on Reddit suggested it and I really don't
    want to charge money for this)

--- src/pkjs/config.js
+++ src/pkjs/config.js
@@ -44,6 +44,19 @@ module.exports = [
     "defaultValue": "Save"
   },
   {
+  "type": "section",
+  "items": [
+      {
+        "type": "text",
+        "defaultValue": "If you'd like to buy me a coffee, here's a PayPal button, but you really don't have to :)"
+      },
+      {
+        "type": "text",
+        "defaultValue": "<form action='https://www.paypal.com/cgi-bin/webscr' method='post' target='_top'><input type='hidden' name='cmd' value='_s-xclick'><input type='hidden' name='hosted_button_id' value='Y7BXBVW4H6FAY'><input type='image' src='https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif' border='0' name='submit' alt='PayPal - The safer, easier way to pay online!'><img alt='' border='0' src='https://www.paypalobjects.com/de_DE/i/scr/pixel.gif' width='1' height='1'></form>"
+      }
+    ]
+  },
+  {
     "type": "heading",
     "defaultValue": "Please remember this disclaimer from Pebble"
   },
